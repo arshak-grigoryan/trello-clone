ef1e2a44f5c9d06a23b6ca16d9fa370a9caafc46
arshak-grigoryan <arshak.code@gmail.com>
2024-04-14 00:44:19 +0400

    fix: husky setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,17 @@
-import { BrowserRouter, HashRouter, Route, Routes } from "react-router-dom";
-import HomePage from "./components/pages/HomePage";
+import { BrowserRouter, HashRouter, Route, Routes } from 'react-router-dom';
+import HomePage from './components/pages/HomePage';
 
 const Router = import.meta.env.DEV ? BrowserRouter : HashRouter;
 
 function App() {
-
   return (
     <Router>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="*" element={<>Not found</>} />
-        </Routes>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="*" element={<>Not found</>} />
+      </Routes>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
